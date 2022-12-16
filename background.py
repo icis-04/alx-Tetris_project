1f027469062ca8ea4ff7aec2bac66557dc3dc5e6
icis-04 <85933343+icis-04@users.noreply.github.com>
2022-12-16 10:26:33 +0100

    update background.py

--- background.py
+++ background.py
@@ -260,14 +260,14 @@ def draw_next_shape(shape, win):
 
 def update_score(nscore):
     score = max_score()
-    with open('scores.txt', 'w') as f:
+    with open('alx-Tetris_project/scores.txt', 'w') as f:
         if int(score) > nscore:
             f.write(str(score))
         else:
             f.write(str(nscore))
 
 def max_score():
-    with open('scores.txt', 'r') as f:
+    with open('alx-Tetris_project/scores.txt', 'r') as f:
         lines = f.readlines()
         score = lines[0].strip()
     return score
